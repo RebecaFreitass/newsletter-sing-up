7e0021acfc4f07ae66f4eda9ccdca09c97b66abb
Rebeca  Freitas <rebecasguerri@gmail.com>
2023-08-23 18:32:41 -0300

    update

--- js.js
+++ js.js
@@ -2,22 +2,30 @@
 const inputEmail = document.getElementById("email-input");
 const span = document.getElementById("error-span")
 const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-console.log(inputEmail, "ola")
+const mainLogin = document.querySelector(".main-login")
+const successPage = document.querySelector(".success-page")
 function setError() {
     inputEmail.style.border ='2px solid #ff0000'
-    span.style.border = "block"
+    span.style.display= "block"
 
 }
 function removeError() {
     inputEmail.style.border =''
-    span.style.border = "none"
+    span.style.display= "none"
 }
 
 function emailValidate(){
-    if(emailRegex.test(inputEmail.value)){
+    if(!emailRegex.test(inputEmail.value)){
         setError()
-        console.log(validado)
+       console.log("naopresta")
     }else{
-        removeError()
+       removeError()
+       console.log("presta")
     }
+}
+
+const submitButton = document.getElementById("submit-button")
+function showtheSuccessPage(){
+    mainLogin.style.display = "none"
+    successPage.style.display = "block"
 }
